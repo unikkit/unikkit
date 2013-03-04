4a64a4af86a76b28fe0c109fa0058e98c5c2423c
rk <rameshkrishnanbj@gmail.com>
2013-03-03 23:06:32 -0500

    Renamed.

---
+++ src/main/java/org/mamce/unikkit/model/staff/Achievement.java
@@ -0,0 +1,56 @@
+package org.mamce.unikkit.model.staff;
+
+import org.mamce.unikkit.model.BaseModel;
+
+/**
+ * @author Ramesh
+ *
+ */
+public class Achievement extends BaseModel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Staff staff;
+	private String type;
+	private String description;
+	
+	/**
+	 * @return the staff
+	 */
+	public Staff getStaff() {
+		return staff;
+	}
+	/**
+	 * @param staff the staff to set
+	 */
+	public void setStaff(Staff staff) {
+		this.staff = staff;
+	}
+	/**
+	 * @return the type
+	 */
+	public String getType() {
+		return type;
+	}
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(String type) {
+		this.type = type;
+	}
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+}
