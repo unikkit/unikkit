1a58e10c92ccbab16c7071b4711c0c9f6da2f9df
rk <rameshkrishnanbj@gmail.com>
2013-03-03 22:59:03 -0500

    Initial version. Columns for staff data import

---
+++ src/main/java/org/mamce/unikkit/xls/StaffImporterColumns.java
@@ -0,0 +1,19 @@
+package org.mamce.unikkit.xls;
+
+public interface StaffImporterColumns {
+
+	int STAFF_NUMBER = 0;
+	int NAME = 1;
+	int DOJ	= 2;
+	int GENDER = 3;
+	int DOB	= 4;
+	int COLLEGE	= 5;
+	int DESIGNATION = 6;
+	int DEPARTMENT = 7;	
+	int ADDRESS1 = 8;
+	int ADDRESS2 = 9;
+	int EMAIL = 10;
+	int CITY = 11;
+	int STATE = 12;
+	int COUNTRY = 13;
+}
