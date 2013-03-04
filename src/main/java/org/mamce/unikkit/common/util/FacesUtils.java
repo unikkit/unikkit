4619e7b93fb4fc182dd8f98d8ea4d9bba278c92d
rk <rameshkrishnanbj@gmail.com>
2013-03-03 23:05:20 -0500

    Added util methods to set attributes in session and request

--- src/main/java/org/mamce/unikkit/common/util/FacesUtils.java
+++ src/main/java/org/mamce/unikkit/common/util/FacesUtils.java
@@ -1,6 +1,31 @@
 package org.mamce.unikkit.common.util;
 
-public class FacesUtils {
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 
+/**
+ * @author Ramesh
+ *
+ */
+public class FacesUtils {
 	
+	public static void setRequestAttribute(String key, Object value) {
+		ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+		externalContext.getRequestMap().put(key, value);
+	}
+	
+	public static void setSessionAttribute(String key, Object value) {
+		ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+		externalContext.getSessionMap().put(key, value);
+	}
+	
+	public static Object getRequestAttribute(String key) {
+		ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+		return externalContext.getRequestMap().get(key);
+	}
+	
+	public static Object getSessionAttribute(String key) {
+		ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+		return externalContext.getSessionMap().get(key);
+	}
 }
