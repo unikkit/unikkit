10c9e0ca43f020ae70e2946f65303e0ecd2417cc
rk <rameshkrishnanbj@gmail.com>
2013-03-22 11:27:10 -0400

    Initial version. Property utils

---
+++ src/main/java/org/mamce/unikkit/common/util/PropertyUtils.java
@@ -0,0 +1,58 @@
+package org.mamce.unikkit.common.util;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
+
+/**
+ * @author Ramesh
+ *
+ */
+public class PropertyUtils {
+
+	public static final Logger LOGGER = Logger.getLogger(PropertyUtils.class);
+	private static Properties props;
+	
+	static {
+		
+		if(props == null) {
+			Resource resource = new ClassPathResource(Constants.UNIKK_PROPS_FILE);
+			try {
+				props = PropertiesLoaderUtils.loadProperties(resource);
+				LOGGER.info("unikk properties loaded!");
+			} catch (IOException e) {
+				LOGGER.fatal("Error loading property file", e);
+			}
+		}
+	
+	}
+	
+	/**
+	 * @param key
+	 * @return
+	 */
+	public static String getProperty(String key) {
+		return props.getProperty(key);
+	}
+	
+	/**
+	 * @param key
+	 * @param defaultValue
+	 * @return
+	 */
+	public static String getProperty(String key, String defaultValue) {
+		return props.getProperty(key, defaultValue);
+	}
+	
+	/**
+	 * @param key
+	 * @return
+	 */
+	public static Object getObject(Object key) {
+		return props.get(key);
+	}
+}
