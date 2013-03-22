fa2a41437cbc64d1c4b8450d450b4c78737c26d4
rk <rameshkrishnanbj@gmail.com>
2013-03-22 11:28:38 -0400

    Added constants and utilty methods

--- src/main/java/org/mamce/unikkit/common/util/Constants.java
+++ src/main/java/org/mamce/unikkit/common/util/Constants.java
@@ -3,4 +3,9 @@ package org.mamce.unikkit.common.util;
 public interface Constants {
 
 	String USER_SESSION_KEY = "loggedInUser";
+	String UNIKK_PROPS_FILE = "/config/unikk.properties";
+	String TIME_FORMAT = "HH:mm a";
+	String DT_FORMAT_DD_MMM_YYYY = "dd MMM yyyy";
+	int ZERO = 0;
+	long UNSAVED_ID = -1;
 }
--- src/main/java/org/mamce/unikkit/common/util/UnikkUtils.java
+++ src/main/java/org/mamce/unikkit/common/util/UnikkUtils.java
@@ -3,8 +3,22 @@ package org.mamce.unikkit.common.util;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.text.MessageFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+/**
+ * @author Ramesh
+ *
+ */
 public class UnikkUtils {
+
+	/**
+	 * @return the today
+	 */
+	public static Date getToday() {
+		return new Date();
+	}
 
 	public static String hashIt(String str) {
 		String md5 = null;
@@ -28,7 +42,82 @@ public class UnikkUtils {
         return md5;
 	}
 	
-	public static void main(String[] args) {
-		System.out.println(hashIt("admin"));
+	public static String getStudentXlsUploadArcLocation(String originalFileName) {
+		String studentArcLoc = PropertyUtils.getProperty("xls.upload.loc.student");
+		String fileName = "ARC_Stud_" + getTimeStamp() + "." + getFileExtension(originalFileName);
+		String fullQualifiedPath = MessageFormat.format(studentArcLoc, fileName);
+		
+		return fullQualifiedPath;
 	}
+	
+	public static String getStaffXlsUploadArcLocation(String originalFileName) {
+		String studentArcLoc = PropertyUtils.getProperty("xls.upload.loc.staff");
+		String fileName = "ARC_Staff_" + getTimeStamp() + "." + getFileExtension(originalFileName);
+		String fullQualifiedPath = MessageFormat.format(studentArcLoc, fileName);
+		
+		return fullQualifiedPath;
+	}
+	
+	public static String getStudentImageUploadLocation(long studentId, String originalFileName) {
+		String studentArcLoc = PropertyUtils.getProperty("img.upload.loc.student");
+		String fileName = studentId + "." + getFileExtension(originalFileName);
+		String fullQualifiedPath = MessageFormat.format(studentArcLoc, fileName);
+		
+		return fullQualifiedPath;
+	}
+	
+	public static String getStaffImageUploadLocation(long staffId, String originalFileName) {
+		String staffArcLoc = PropertyUtils.getProperty("img.upload.loc.staff");
+		String fileName = staffId + "." + getFileExtension(originalFileName);
+		String fullQualifiedPath = MessageFormat.format(staffArcLoc, fileName);
+		
+		
+		return fullQualifiedPath;
+	}
+	
+	public static String getTimeStamp() {
+		return String.valueOf(new Date().getTime());
+	}
+	
+	public static String getFileExtension(String fileName) {
+		String ext = "";
+		if(fileName != null) {
+			int dotIndex = fileName.lastIndexOf(".");
+			
+			if(dotIndex > 0) {
+				ext = fileName.substring(dotIndex + 1);
+			}
+		}
+		
+		return ext;
+	}
+	
+	public static int getMaxLatestEventCount() {
+		String maxCount = PropertyUtils.getProperty("max.dashboard.event.display");
+		int max = 0;
+		if(!"".equals(maxCount)) {
+			max = Integer.valueOf(maxCount).intValue();
+		} else {
+			max = 50;
+		}
+		
+		return max;
+	}
+	
+	public static String getTimeFromDate(Date date) {
+		SimpleDateFormat sdf = new SimpleDateFormat(Constants.TIME_FORMAT);
+		
+		if(date == null) return "";
+		
+		return sdf.format(date);
+	}
+	
+	public static String getReadableDateStr(Date date) {
+		SimpleDateFormat sdf = new SimpleDateFormat(Constants.DT_FORMAT_DD_MMM_YYYY);
+		
+		if(date == null) return "";
+		
+		return sdf.format(date);
+	}
+	
 }
