7c6e847dc783ebb66da25e98770e2fe489c3237c
rk <rameshkrishnanbj@gmail.com>
2013-03-22 11:25:25 -0400

    Updated event lazy implementation

--- src/main/java/org/mamce/unikkit/lazy/model/EventLazyModel.java
+++ src/main/java/org/mamce/unikkit/lazy/model/EventLazyModel.java
@@ -1,23 +1,63 @@
 package org.mamce.unikkit.lazy.model;
 
-import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
-import org.primefaces.model.DefaultScheduleEvent;
+import org.mamce.unikkit.event.manager.EventManager;
 import org.primefaces.model.LazyScheduleModel;
-
+import org.primefaces.model.ScheduleEvent;
 
 public class EventLazyModel extends LazyScheduleModel {
+
+	private EventManager eventManager;
+	
+	/**
+	 * @return the eventManager
+	 */
+	public EventManager getEventManager() {
+		return eventManager;
+	}
+
+	/**
+	 * @param eventManager the eventManager to set
+	 */
+	public void setEventManager(EventManager eventManager) {
+		this.eventManager = eventManager;
+	}
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
-    public void loadEvents(Date start, Date end) {  
+    public void loadEvents(Date start, Date end) {
         clear();
-        Calendar date = Calendar.getInstance();
-        addEvent(new DefaultScheduleEvent("Lazy Event 1", date.getTime(), date.getTime()));  
+        getEvents().addAll(eventManager.getScheduledEvents());
     }
 	
+    @Override
+    public boolean deleteEvent(ScheduleEvent event) {
+    	// TODO Auto-generated method stub
+    	return super.deleteEvent(event);
+    }
+    
+    @Override
+    public ScheduleEvent getEvent(String id) {
+    	// TODO Auto-generated method stub
+    	return super.getEvent(id);
+    }
+    
+    @Override
+    public void updateEvent(ScheduleEvent event) {
+    	// TODO Auto-generated method stub
+    	super.updateEvent(event);
+    }
+    
+    @Override
+    public List<ScheduleEvent> getEvents() {
+    	// TODO Auto-generated method stub
+    	return super.getEvents();
+    }
 }
+
+
