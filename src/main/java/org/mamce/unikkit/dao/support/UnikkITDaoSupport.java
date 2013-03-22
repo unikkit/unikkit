f24cbb5386599b2bc73bf76c07fc04ad81760f94
rk <rameshkrishnanbj@gmail.com>
2013-03-22 11:31:20 -0400

    Added delete object method and clean up date usage

--- src/main/java/org/mamce/unikkit/dao/support/UnikkITDaoSupport.java
+++ src/main/java/org/mamce/unikkit/dao/support/UnikkITDaoSupport.java
@@ -1,9 +1,9 @@
 package org.mamce.unikkit.dao.support;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
+import org.mamce.unikkit.common.util.UnikkUtils;
 import org.mamce.unikkit.model.BaseModel;
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 
@@ -14,8 +14,6 @@ import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
  */
 public class UnikkITDaoSupport<T extends BaseModel> extends HibernateDaoSupport {
 
-	private Date currentDate = new Date();
-	
 	protected T get(Class<? extends BaseModel> clazz, long id) {
 		return (T) getHibernateTemplate().get(clazz, id);
 	}
@@ -25,10 +23,7 @@ public class UnikkITDaoSupport<T extends BaseModel> extends HibernateDaoSupport 
 	}
 	
 	protected void saveObject(T t) {
-		if(t.getId() < 0) {
-			t.setCreatedDate(currentDate);
-		}
-		t.setUpdatedDate(currentDate);
+		t.setUpdatedDate(UnikkUtils.getToday());
 		
 		getHibernateTemplate().saveOrUpdate(t);
 	}
@@ -37,14 +32,15 @@ public class UnikkITDaoSupport<T extends BaseModel> extends HibernateDaoSupport 
 		List<T> savableEntities = new ArrayList<>();
 		
 		for (T entity : entities) {
-			if(entity.getId() < 0) {
-				entity.setCreatedDate(currentDate);
-			}
-			entity.setUpdatedDate(currentDate);
+			entity.setUpdatedDate(UnikkUtils.getToday());
 			savableEntities.add(entity);
 		}
 		
 		getHibernateTemplate().saveOrUpdateAll(savableEntities);
+	}
+	
+	protected void deleteObject(T t) {
+		getHibernateTemplate().delete(t);
 	}
 	
 }
